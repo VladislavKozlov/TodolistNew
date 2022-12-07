96c05e2c8ae358160f72ef54c4392efb4b807411
VladislavKozlov <k2vakosa@yandex.ru>
2022-12-07 05:06:14 +0400

    Small fix for normal startup

--- Todolist/App_Start/BundleConfig.cs
+++ Todolist/App_Start/BundleConfig.cs
@@ -11,7 +11,7 @@ namespace Todolist
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-3.3.1.js",
                         "~/Scripts/jquery.unobtrusive-ajax*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
